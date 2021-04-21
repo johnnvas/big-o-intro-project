bc34ed18e377a1829f3fd6cc4279bd541ffa87ad
Johnn <j.christian.vasquez@gmail.com>
2021-04-21 16:45:06 -0500

    phase 2

--- 02-anagrams.js
+++ 02-anagrams.js
@@ -1,7 +1,19 @@
 function firstAnagram(str1, str2) {
   // Code goes here ....
-}
+  let arr = str2.split('')
+  for (let i = 0; i <= str1.length - 1; i++) {
+    let letter = str1[i]
+    if (arr.includes(letter)) {
+      arr.splice(str2.indexOf(letter), 1)
+    } else {
+      return false;
+    }
+  }
+  if (!arr.length) return true;
 
+}
+console.log(firstAnagram("gizmo", "sally"));    // => false
+console.log(firstAnagram("elvis", "lives"));    // => true
 
 
 function secondAnagram(str1, str2) {
