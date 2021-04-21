a90f930a99223f49ab906be59463c16d1e7a6323
Johnn <j.christian.vasquez@gmail.com>
2021-04-21 15:55:40 -0500

    first commit

--- 01-big-octopus.js
+++ 01-big-octopus.js
@@ -1,16 +1,42 @@
-function quadraticBiggestFish(fishes) {
-  // Code goes here ...
-}
+const fishes = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish',
+'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh'];
+
+// function quadraticBiggestFish(fishes) {
+//   // Code goes here ...
+//   let biggestFish;
+//   for (let i = 0; i <= fishes.length - 1; i++) {
+//     for (let j = i + 1; j <= fishes.length - 1; j++) {
+//       if (fishes[j].length > fishes[i].length) {
+//       biggestFish = fishes[j];
+//       }
+//     }
+//   }
+//   console.log(biggestFish)
+//   return biggestFish;
+// }
+
+// quadraticBiggestFish(fishes)
 
 
-function nlognBiggestFish(fishes) {
-  // Code goes here ...
-}
+// function nlognBiggestFish(fishes) {
+//   // Code goes here ...
+//   fishes.sort((a, b)=> a.length - b.length);
+//   console.log(fishes[fishes.length - 1])
+//   return (fishes[fishes.length - 1])
+// }
+// nlognBiggestFish(fishes)
 
 
 function linearBiggestFish(fishes) {
   // Code goes here ...
+  let biggestFish = fishes[0]
+  for (let i = 0; i <= fishes.length - 1; i++) {
+    if (fishes[i].length > biggestFish.length) biggestFish = fishes[i];
+  }
+   console.log(biggestFish);
+    return biggestFish;
 }
+linearBiggestFish(fishes)
 
 
 tilesArray = ["up", "right-up", "right", "right-down", "down", "left-down", "left", "left-up"]
